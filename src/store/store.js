c55294312114a6b0cd497078d20724194ef128d9
andreaangelova <andrea.angelova@hotmail.com>
2021-08-13 20:23:52 +0200

    add redux dev tools

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
 import appReducer from "./reducer";
 
-const store = createStore(appReducer);
+const store = createStore(appReducer, window.__REDUX_DEVTOOLS_EXTENSION__());
 export default store;
