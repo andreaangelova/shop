a2a987c1c7f0cb3ec79cb3b700720fe20e86340f
andreaangelova <andrea.angelova@hotmail.com>
2021-08-13 19:16:41 +0200

    add alert and shopping card

--- src/components/pages/Shop.js
+++ src/components/pages/Shop.js
@@ -1,21 +1,56 @@
-import { Button, Card } from "react-bootstrap";
-import image from "./../../images/image1.jpeg";
+import { useState } from "react";
+import { Button, Card, Col, Row, Alert } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
 const Shop = () => {
+  const history = useHistory();
+  const [show, setShow] = useState(false);
+  const logIn = () => {
+    setShow(false);
+    history.push("/login");
+  };
   return (
     <div>
-      <h1>This is shop page</h1>
-      <Card className="card" style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={image} />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+      <Alert show={show} variant="success" className="alert">
+        <Alert.Heading>How's it going?!</Alert.Heading>
+        <p>To add to a crad you need to be logged in</p>
+        <hr />
+        <div className="d-flex justify-content-end">
+          <Button onClick={() => setShow(false)} variant="outline-danger">
+            Cancel
+          </Button>
+          <Button onClick={logIn} variant="outline-success">
+            Log In
+          </Button>
+        </div>
+      </Alert>
+      <div className={show ? "blur" : ""}>
+        <h1>This is shop page</h1>
+        <Row xs={1} md={4} className="g-4 margin-left10">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((item) => {
+            return (
+              <Col key={item}>
+                <Card className="card" style={{ width: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src={require(`./../../images/image${item}.jpeg`).default}
+                  />
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary" onClick={() => setShow(true)}>
+                      Add to card
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </div>
     </div>
   );
 };
