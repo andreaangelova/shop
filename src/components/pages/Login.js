2814a2ecd6849e86be45f6f338bcdeac29c8a0e9
andreaangelova <andrea.angelova@hotmail.com>
2021-08-13 19:27:15 +0200

    add log in page

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,5 +1,56 @@
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+
 const Login = () => {
-  return <h1>Log in to shop!</h1>;
+  const history = useHistory();
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [submitted, setSubmitted] = useState(false);
+  const submit = (e) => {
+    e.preventDefault();
+    if (formData.email && formData.password) {
+      history.push("/");
+    }
+    setSubmitted(true);
+  };
+  return (
+    <div>
+      <h1>Log in to shop!</h1>
+      <br></br>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(e) =>
+              setFormData({ ...formData, password: e.target.value })
+            }
+          />
+        </Form.Group>
+        {submitted && (!formData.email || !formData.password) && (
+          <p>Please enter valid email and password</p>
+        )}
+        <Button variant="primary" type="submit" onClick={(e) => submit(e)}>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default Login;
